cce2374dfa08a1cd9999620c9cfa553bdd6f53ca
Min <min.rai@itonics.de>
2019-10-25 15:18:28 +0545

    git init project

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+// Webpack uses this to work with directories
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+// This is main configuration object.
+// Here you write different options and tell Webpack what to do
+module.exports = (env, argv) => {
+
+    const config = {
+    // Path to your entry point. From this file Webpack will begin his work
+    entry:  './assets/src/js/index.js',
+
+    // Path and filename of your result bundle.
+    // Webpack will bundle all JavaScript into this file
+    output: {
+        path: path.resolve(__dirname, 'assets/dist'),
+        filename: 'development' === argv.mode ?  '[name].js' : '[name].min.js'
+    },
+    module: {
+        rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                        presets: ['@babel/preset-env']
+                        }
+                    }
+                },
+                {
+                    // Apply rule for .sass, .scss or .css files
+                    test: /\.(sa|sc|c)ss$/,
+                    // Set loaders to transform files.
+                    // Loaders are applying from right to left(!)
+                    // The first loader will be applied after others
+                    use: [
+                        {
+                            // After all CSS loaders we use plugin to do his work.
+                            // It gets all transformed CSS and extracts it into separate
+                            // single bundled file
+                            loader: MiniCssExtractPlugin.loader
+                        }, 
+                        {
+                            // This loader resolves url() and @imports inside CSS
+                            loader: "css-loader",
+                        },
+                        {
+                            // Then we apply postCSS fixes like autoprefixer and minifying
+                            loader: "postcss-loader"
+                        }, 
+                        {
+                            // First we transform SASS to standard CSS
+                            loader: "sass-loader",
+                            options: {
+                                implementation: require("sass")
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'development' === argv.mode ? "style.css" : "style.min.css"
+            })
+        ]
+    };
+
+    return config;
+
+}
